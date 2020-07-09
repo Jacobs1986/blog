358f11646ee7927605c5da5e3a4fd7e2f55da197
Sean Jacobson <sean.jacobson86@gmail.com>
2020-07-08 18:03:04 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
     return (
-        <div>
-            Testing to make sure this is working.
-        </div>
+        <Container fluid>
+            <Row>
+                <h1>Blog title</h1>
+            </Row>
+            <Row>
+                <Col md={2}>
+                    Links to past posts go here.
+                </Col>
+                <Col md={7}>
+                    Posts go here.
+                </Col>
+                <Col md={3}>
+                    Pictures can go here.
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
